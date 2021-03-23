b78673d1496977642c90bae0c9d55ba531b6cf78
cgkrishnanunni <67425535+cgkrishnanunni@users.noreply.github.com>
2021-03-22 20:52:00 -0500

    Nil

---
+++ Sparse training_additive/Utilities/additive_output.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, BatchNormalization
+from tensorflow.keras.initializers import RandomNormal, RandomUniform, Constant
+import numpy as np
+import pandas as pd
+import pdb #Equivalent of keyboard in MATLAB, just add "pdb.set_trace()"
+from Utilities.Net import Final_Network
+
+
+# In[ ]:
+
+
+def net_output(hyperp,data, run_options, data_input_shape, label_dimensions,i_val,batch_pred_test):
+    
+    y_pred_test_add=0*batch_pred_test
+    if i_val>1:
+        for i_net in range(2,i_val+1):
+                
+                
+            Network=Final_Network(hyperp, run_options, data_input_shape, label_dimensions) 
+        
+            Network.load_weights("WEIGHTS"+'/'+"model_weights"+str(i_net-1)).expect_partial()
+    
+            y_pred_test_add=y_pred_test_add+Network(data,hyperp)
+        
+    return y_pred_test_add
+
